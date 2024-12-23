44cde8122ecc8f196c51b64f0ccfbdc2fd1e70b6
Jake Walter <jakewalter080@gmail.com>
2024-12-23 18:53:59 -0500

    add file structure schema

--- vite-project/server/models/coffeeRegion.js
+++ vite-project/server/models/coffeeRegion.js
@@ -1,9 +1,29 @@
 const { Schema, model } = require('mongoose');
+const { beanProduction } = require('../seeds/coffeeData/asia/india');
 
 // I believe this is where we will match the API data to the schema
 const coffeeRegionSchema = new Schema({
-    country: {
+    countryCode: {
         type: String,
         requireds: true,
         unique: true
+    },
+    country: {
+        type: String,
+        requireds: true,
+    },
+    output: {
+        percentage: String
+    },
+    coffeeHistory: {
+        discovery: String,
+        development: String,
+    },
+    beanProduction: {
+        flavorProfiles: String,
+        regions: [String]
+    },
+    brewingMethods: {
+        traditional: String,
+        modern: String
     },
