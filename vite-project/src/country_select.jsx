d19078828182e6451166d0adb26390379eaa0ef2
Jacob Rowe <jrowe1970@att.net>
2024-12-23 19:27:37 -0500

    added filter to countries

--- vite-project/src/country_select.jsx
+++ vite-project/src/country_select.jsx
@@ -10,7 +10,7 @@ const client = new ApolloClient({
 // GraphQL query to fetch countries' names and codes
 const LIST_COUNTRIES = gql`
   {
-    countries {
+    countries (filter: { code: { in: ["US", "CA", "MX", "ET", "CI", "KE", "IN", "ID", "FR", "GR", "IT", "TR", "YE", "BR", "CO", "CU", "HN", "PE", "GT", "CR"] } }) {
       name
       code
     }
