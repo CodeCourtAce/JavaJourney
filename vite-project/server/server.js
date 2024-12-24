256921501c7efd140e51a6e7b143e69ee1c93cfd
Jake Walter <jakewalter080@gmail.com>
2024-12-23 20:24:50 -0500

    update server.js

--- vite-project/server/server.js
+++ vite-project/server/server.js
@@ -1,32 +1,35 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
+import { typeDefs, resolvers } from './schemas/index.js';
+import db from './config/connection.js';
 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers,
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const startApolloServer = async () => {
-    await server.start();
-    server.applyMiddleware({ app });
-}
+  await server.start();
+  server.applyMiddleware({ app });
 
-db.once('open', () => {
+  db.once('open', () => {
     app.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
-        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     });
-});
+  });
+};
 
 startApolloServer();
